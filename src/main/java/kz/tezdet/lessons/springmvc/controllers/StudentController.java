6c714b50789c0ec6c402ac04e45ed4517b9ea458
daulet <daucloudlab@gmail.com>
2016-04-13 14:10:20 +0600

    version 02

--- src/main/java/kz/tezdet/lessons/springmvc/controllers/StudentController.java
+++ src/main/java/kz/tezdet/lessons/springmvc/controllers/StudentController.java
@@ -1,11 +1,12 @@
 package kz.tezdet.lessons.springmvc.controllers;
 
 import kz.tezdet.lessons.springmvc.models.Student;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,5 +21,14 @@ public class StudentController {
         map.put("status", "OK") ;
         map.put("student", student) ;
         return map ;
+    }
+
+    @RequestMapping(value = "/student/", method = RequestMethod.POST)
+    public ResponseEntity<Void> createStudent(@RequestBody Student student,
+                                              UriComponentsBuilder uriComponentsBuilder){
+        HttpHeaders httpHeaders = new HttpHeaders() ;
+        httpHeaders.setLocation(uriComponentsBuilder.path("/student/{id}")
+            .buildAndExpand(student.getStudentID()).toUri());
+        return new ResponseEntity<Void>(httpHeaders, HttpStatus.CREATED) ;
     }
 }
